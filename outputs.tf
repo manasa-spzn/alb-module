ee25893d713ad87ab8155f0ce4159950eca81aaa
Manasa <6manasak@gmail.com>
2025-12-15 21:34:41 -0500

    added target group arn output

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,8 @@ output "alb_url" {
   description = "URL of the ALB"
   value = "http://${aws_lb.my_alb.dns_name}"
 }
+
+output "target_group_arn" {
+  description = "ARN of the ALB target group"
+  value       = aws_lb_target_group.alb_target_group.arn
+}
